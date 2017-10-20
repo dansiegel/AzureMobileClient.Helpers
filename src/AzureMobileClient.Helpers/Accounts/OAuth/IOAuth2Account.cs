00db74fc5654aca29ae0dbb248efa7dfdc8ed106
Dan Siegel <me@dansiegel.net>
2017-10-19 20:44:00 -0700

    removes member that hides Account.AccessToken

--- src/AzureMobileClient.Helpers/Accounts/OAuth/IOAuth2Account.cs
+++ src/AzureMobileClient.Helpers/Accounts/OAuth/IOAuth2Account.cs
@@ -2,18 +2,39 @@
 
 namespace AzureMobileClient.Helpers.Accounts.OAuth
 {
+    /// <summary>
+    /// OAuth2 account.
+    /// </summary>
     public interface IOAuth2Account : IAccount
     {
-        string AccessToken { get; set; }
-
+        /// <summary>
+        /// Gets or sets the refresh token.
+        /// </summary>
+        /// <value>The refresh token.</value>
         string RefreshToken { get; set; }
 
+        /// <summary>
+        /// Gets or sets the type of the token.
+        /// </summary>
+        /// <value>The type of the token.</value>
         string TokenType { get; set; }
 
+        /// <summary>
+        /// Gets or sets the scope.
+        /// </summary>
+        /// <value>The scope.</value>
         string Scope { get; set; }
 
+        /// <summary>
+        /// Gets or sets the access token expires.
+        /// </summary>
+        /// <value>The access token expires.</value>
         DateTime? AccessTokenExpires { get; set; }
 
+        /// <summary>
+        /// Gets or sets the refresh token expires.
+        /// </summary>
+        /// <value>The refresh token expires.</value>
         DateTime? RefreshTokenExpires { get; set; }
     }
 }
--- src/AzureMobileClient.Helpers/Accounts/OAuth/OAuth2Account.cs
+++ src/AzureMobileClient.Helpers/Accounts/OAuth/OAuth2Account.cs
@@ -2,44 +2,66 @@
 
 namespace AzureMobileClient.Helpers.Accounts.OAuth
 {
+    /// <summary>
+    /// OAuth2 Account.
+    /// </summary>
     public class OAuth2Account : Account, IOAuth2Account
     {
-        public override string AccessToken
-        {
-            get { return GetStringValue("access_token"); }
-            set { SetStringValue("access_token", value); }
-        }
-
+        /// <summary>
+        /// Gets or sets the refresh token.
+        /// </summary>
+        /// <value>The refresh token.</value>
         public string RefreshToken
         {
             get { return GetStringValue("refresh_token"); }
             set { SetStringValue("refresh_token", value); }
         }
 
+        /// <summary>
+        /// Gets or sets the type of the token.
+        /// </summary>
+        /// <value>The type of the token.</value>
         public string TokenType
         {
             get { return GetStringValue("token_type"); }
             set { SetStringValue("token_type", value); }
         }
 
+        /// <summary>
+        /// Gets or sets the scope.
+        /// </summary>
+        /// <value>The scope.</value>
         public string Scope
         {
             get { return GetStringValue("scope"); }
             set { SetStringValue("scope", value); }
         }
 
+        /// <summary>
+        /// Gets or sets the access token expires.
+        /// </summary>
+        /// <value>The access token expires.</value>
         public DateTime? AccessTokenExpires
         {
             get { return GetDateTimeValue("expires_in"); }
             set { SetDateTimeValue("expires_in", value); }
         }
 
+        /// <summary>
+        /// Gets or sets the refresh token expires.
+        /// </summary>
+        /// <value>The refresh token expires.</value>
         public DateTime? RefreshTokenExpires
         {
             get { return GetDateTimeValue("refresh_expires_in"); }
             set { SetDateTimeValue("refresh_expires_in", value); }
         }
 
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="T:AzureMobileClient.Helpers.Accounts.OAuth.OAuth2Account"/>
+        /// is valid.
+        /// </summary>
+        /// <value><c>true</c> if is valid; otherwise, <c>false</c>.</value>
         public override bool IsValid
         {
             get
