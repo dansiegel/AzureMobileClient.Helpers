8e1d8c8c7463b1014a1162c88116a8e04489ed7b
Dan Siegel <me@dansiegel.net>
2017-10-19 21:05:14 -0700

    adding missing xml docs

--- src/AzureMobileClient.Helpers/Accounts/IAccount.cs
+++ src/AzureMobileClient.Helpers/Accounts/IAccount.cs
@@ -6,13 +6,39 @@ using System.Threading.Tasks;
 
 namespace AzureMobileClient.Helpers.Accounts
 {
-
+    /// <summary>
+    /// Account.
+    /// </summary>
     public interface IAccount : IDictionary<string, string>
     {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
         string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>The name.</value>
         string Name { get; set; }
+
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="T:AzureMobileClient.Helpers.Accounts.IAccount"/> is valid.
+        /// </summary>
+        /// <value><c>true</c> if is valid; otherwise, <c>false</c>.</value>
         bool IsValid { get; }
+
+        /// <summary>
+        /// Gets or sets the access token.
+        /// </summary>
+        /// <value>The access token.</value>
         string AccessToken { get; set; }
+
+        /// <summary>
+        /// Checks the validity.
+        /// </summary>
+        /// <returns>The validity.</returns>
         Task<bool> CheckValidity();
     }
 }
--- src/AzureMobileClient.Helpers/Accounts/JsonSerializer.cs
+++ src/AzureMobileClient.Helpers/Accounts/JsonSerializer.cs
@@ -3,8 +3,19 @@ using System.IO;
 
 namespace AzureMobileClient.Helpers.Accounts
 {
+    /// <summary>
+    /// Json serializer.
+    /// </summary>
     public static class JsonSerializer
-	{
+    {
+        /// <summary>
+        /// Deserialize the specified json, throwOnFailure and withTypes.
+        /// </summary>
+        /// <returns>The deserialize.</returns>
+        /// <param name="json">Json.</param>
+        /// <param name="throwOnFailure">If set to <c>true</c> throw on failure.</param>
+        /// <param name="withTypes">If set to <c>true</c> with types.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
         public static T Deserialize<T>(string json, bool throwOnFailure = false, bool withTypes = false)
         {
             var settings = new Newtonsoft.Json.JsonSerializerSettings();
@@ -25,6 +36,14 @@ namespace AzureMobileClient.Helpers.Accounts
             return default(T);
         }
 
+        /// <summary>
+        /// Serialize the specified obj, throwOnFailure and withTypes.
+        /// </summary>
+        /// <returns>The serialize.</returns>
+        /// <param name="obj">Object.</param>
+        /// <param name="throwOnFailure">If set to <c>true</c> throw on failure.</param>
+        /// <param name="withTypes">If set to <c>true</c> with types.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
         public static string Serialize<T>(T obj, bool throwOnFailure = false, bool withTypes = false)
         {
             var settings = new Newtonsoft.Json.JsonSerializerSettings();
